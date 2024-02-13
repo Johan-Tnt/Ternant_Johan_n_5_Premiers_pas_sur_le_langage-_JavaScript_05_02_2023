9344ecdd855f305b6c3a3b856649ea5792f65392
Johan <144804317+Johan-Tnt@users.noreply.github.com>
2024-02-13 15:41:59 +0100

    Add bullet point.

--- assets/script.js
+++ assets/script.js
@@ -17,11 +17,31 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
-
+/*Ajout de code Johan*/
+/*Ajout de event listenner arrow left*/
 arrow_left.addEventListener("click", () => {
   console.log("Vous avez cliqué sur le bouton gauche");
 });
-
+/*Ajout de event listenner arrow right*/
 arrow_right.addEventListener("click", () => {
   console.log("Vous avez cliqué sur le bouton droit");
 });
+
+/**Global **/
+const dots = document.querySelector(".dots");
+let index = 0;
+
+/*Ajout des bullet point*/
+function displayDots() {
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dots.appendChild(dot);
+    if (i == index) {
+      dot.classList.add("dot_selected");
+    }
+  }
+}
+displayDots();
+
+/*Ajout du carroussel*/
